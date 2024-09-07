9dc027e52e09d63898821ee720c6d8e3b9149be9
halink0803 <halink0803@gmail.com>
2024-09-07 23:37:31 +0700

    update getUnsealDecryptKey func

--- contracts/onlyfhe.sol
+++ contracts/onlyfhe.sol
@@ -5,32 +5,32 @@ pragma solidity >=0.8.13 <0.9.0;
 import "@fhenixprotocol/contracts/FHE.sol";
 import {Permissioned, Permission} from "@fhenixprotocol/contracts/access/Permissioned.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract OnlyFHE is ERC721Enumerable, Ownable {
+contract OnlyFHE is ERC721Enumerable, Ownable, Permissioned {
 
-    bool paused;
-
-    string basedURI;
-
-    mapping (uint256 => euint128) encryptKey;
+    bool _paused;
+    string _basedURI;
+    uint256 public maxTokenIds = 10;
+    mapping (uint256 => euint128) encryptKeys;
 
     // total number of tokenIds minted
     uint256 public tokenIds;
 
     constructor() ERC721("OnlyFHE", "ONF") Ownable(msg.sender) {}
 
-    function mint() {
+    function mint() public {
         require(tokenIds < maxTokenIds, "Exceed maximum supply");
-        require(msg.value >= _price, "Ether sent is not correct");
         tokenIds += 1;
         _safeMint(msg.sender, tokenIds);
     }
-    function getUnsealDecryptKey(
-        Permission memory permission
-    ) public view onlySender(permission) returns (string) {
-        return FHE.sealoutput()
+
+    function getUnsealDecryptKey(Permission memory permission, uint256 tokenId) public view 
+        onlySender(permission) returns (string memory) {
+        euint128 encryptedKey = encryptKeys[tokenId];
+        return FHE.sealoutput(encryptedKey, permission.publicKey);
     }
 
 }
